2276a4afee6f7fa2c865c3d298b99b8239298330
Thomas4897 <t.maynard4897@gmail.com>
2021-11-17 00:34:03 +0800

    Adding Metronome Counter

--- main.js
+++ main.js
@@ -6,8 +6,17 @@ const tock = new Audio("sounds/tock.mp3");
 
 let tickCounter = 0;
 
+//! Define the counter for metronome-counter
+const counter = document.querySelector(".counter");
+
 //! This function is called every 600ms
 function update() {
+	tickCounter++;
+	//! Count the ticks and tock and reset after 4 counts
+	if (tickCounter < 5) {
+		counter.innerHTML = tickCounter;
+	}
+
 	//! Play the 'tock' every fouth beat
 	if (tickCounter > 3) {
 		tock.play();
@@ -17,7 +26,6 @@ function update() {
 	//! Play the 'tick' sound
 	if (tickCounter < 4) {
 		tick.play();
-		tickCounter++;
 	}
 }
 
