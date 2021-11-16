7bf31c307921ce3091f58506c4f156f13935c53e
Thomas4897 <t.maynard4897@gmail.com>
2021-11-17 01:37:47 +0800

    Adding Drum Maching Sounds to js

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
 //! Setup 'tick' sound
 const tick = new Audio("sounds/tick.mp3");
 
-//! Setup 'tock sound
+//! Setup 'tock' sound
 const tock = new Audio("sounds/tock.mp3");
+
+//! Setup 'kick-drum' sound
+const kickDrum = new Audio("sounds/kick-drum.mp3");
+
+//! Setup 'kick-drum' sound
+const snareDrum = new Audio("sounds/snare-drum.mp3");
+
+//! Setup 'kick-drum' sound
+const hiHat = new Audio("sounds/hi-hat.mp3");
 
 let tickCounter = 0;
 
 //! Define the counter for metronome-counter
 const counter = document.querySelector(".counter");
+
+//! Define the counter for metronome-counter
+const metronomeCheck = document.querySelector("#metronome-drum");
+
+const kickDrumCheck = document.querySelector("#kick-drum");
+
+const snareDrumCheck = document.querySelector("#snare-drum");
+
+const hiHatCheck = document.querySelector("#hi-hat");
 
 //! This function is called every 600ms
 function update() {
@@ -17,13 +35,31 @@ function update() {
 		counter.innerHTML = tickCounter;
 	}
 
+	if (kickDrumCheck.checked) {
+		kickDrum.play();
+	}
+
+	if (snareDrumCheck.checked) {
+		snareDrum.play();
+	}
+
+	if (hiHatCheck.checked) {
+		hiHat.play();
+	}
+
 	//! Play the 'tick' sound for three beats
 	//! Play the 'tock' every fouth beat
-	if (tickCounter % 4 === 0) {
-		tock.play();
-		tickCounter = 0;
+	if (metronomeCheck.checked) {
+		if (tickCounter % 4 === 0) {
+			tock.play();
+			tickCounter = 0;
+		} else {
+			tick.play();
+		}
 	} else {
-		tick.play();
+		if (tickCounter % 4 === 0) {
+			tickCounter = 0;
+		}
 	}
 }
 
