0f660df435dc7b4ef69b8bff3b373c21279a420f
Thomas4897 <t.maynard4897@gmail.com>
2021-11-17 00:19:35 +0800

    Adding Tock Sound

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
-// Setup 'tick' sound
-const tick = new Audio('sounds/tick.mp3');
+//! Setup 'tick' sound
+const tick = new Audio("sounds/tick.mp3");
 
-// This function is called every 600ms
+//! Setup 'tock sound
+const tock = new Audio("sounds/tock.mp3");
+
+let tickCounter = 0;
+
+//! This function is called every 600ms
 function update() {
+	//! Play the 'tock' every fouth beat
+	if (tickCounter > 3) {
+		tock.play();
+		tickCounter = 0;
+	}
 
-    // Play the 'tick' sound
-    tick.play();
+	//! Play the 'tick' sound
+	if (tickCounter < 4) {
+		tick.play();
+		tickCounter++;
+	}
 }
 
-// This function sets up update() to be called every 600ms
+//! This function sets up update() to be called every 600ms
 function setupUpdate() {
-    setInterval(update, 600);
+	setInterval(update, 600);
 }
 
-// Call setupUpdate() once after 300ms
+//! Call setupUpdate() once after 300ms
 setTimeout(setupUpdate, 300);
